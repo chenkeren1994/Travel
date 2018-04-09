60a60422a287ff7d6d9ec7f221ac8dedfedf8f28
seal <chenkerenqiqi@163.com>
2018-04-09 16:56:12 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+        <swiper-slide v-for="item in swiperList" :key="item.id">
+            <img class="swiper-img" :src="item.imgUrl" alt="">
+        </swiper-slide>
+        <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/qs/1803/36/6d3d527ae87bb702.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/qs/1804/99/86c9a8d220f4c902.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background #fff !important
+    .wrapper
+        width 100%
+        height 0
+        overflow hidden
+        padding-bottom 31.25%
+        background #eee
+        .swiper-img{
+            width 100%
+            display block
+        }
+</style>
