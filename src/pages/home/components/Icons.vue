2fdf9525ffd6cab2878e83f0f5d79ae1a751bf49
seal <chenkerenqiqi@163.com>
2018-04-09 19:51:44 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="icons">
+    <swiper>
+        <swiper-slide v-for="(page, index) in pages" :key="index">
+            <div
+                class="icon"
+                v-for="item in page"
+                :key="item.id"
+            >
+                <div class="icon-img">
+                    <img class="icon-img-content" :src="item.imgUrl" :alt="item.desc">
+                </div>
+                <p class="icon-desc">{{item.desc}}</p>
+            </div>
+        </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点门票'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png',
+          desc: '名胜古迹'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/c1/ac058f238b9a6802.png',
+          desc: '一日游'
+        },
+        {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '踏青赏花'
+        },
+        {
+          id: '0005',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/fa/fe1915e1d20bcc02.png',
+          desc: '定制游'
+        },
+        {
+          id: '0006',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '动植物园'
+        },
+        {
+          id: '0007',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png',
+          desc: '故宫'
+        },
+        {
+          id: '0008',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/67/9a1678221b8e0e02.png',
+          desc: '古北水镇'
+        },
+        {
+          id: '0009',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/e3/67df61427c8e1302.png',
+          desc: '演出'
+        },
+        {
+          id: '0010',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png',
+          desc: '自然风光'
+        }
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+@import '~styles/mixins.styl'
+    .icons >>> .swiper-container
+        height 0
+        padding-bottom 50%
+    .icon
+        height 0
+        position relative
+        overflow hidden
+        float left
+        width 25%
+        padding-bottom 25%
+        .icon-img
+            position absolute
+            left 0
+            top 0
+            right 0
+            bottom .44rem
+            box-sizing border-box
+            padding .3rem
+            .icon-img-content
+                display block
+                margin 0 auto
+                height 100%
+        .icon-desc
+            position absolute
+            left 0
+            bottom  0
+            right 0
+            height .44rem
+            line-height .44rem
+            text-align center
+            color $darkTextColor
+            ellipsis()
+</style>
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -21,11 +21,11 @@ export default {
       swiperList: [
         {
           id: '0001',
-          imgUrl: 'http://img1.qunarzz.com/qs/1803/36/6d3d527ae87bb702.jpg'
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1802/73/d1ba8edc561d1f02.jpg_640x200_a747973f.jpg'
         },
         {
           id: '0002',
-          imgUrl: 'http://img1.qunarzz.com/qs/1804/99/86c9a8d220f4c902.jpg'
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1802/51/e78f936a5b404102.jpg_640x200_c14f0b3a.jpg'
         }
       ]
     }
