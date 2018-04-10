b49a586da8f6585c7893581300def6de13fe1e01
seal <chenkerenqiqi@163.com>
2018-04-10 14:37:10 +0800

    performance

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,11 @@
     <div>
       <city-header></city-header>
       <city-search></city-search>
-      <city-list :cities="cities" :hot="hotCities"></city-list>
-      <city-alphabet :cities="cities"></city-alphabet>
+      <city-list :cities="cities" :hot="hotCities" :letter="letter"></city-list>
+      <city-alphabet
+        :cities="cities"
+        @change="handleLetterChange"
+      ></city-alphabet>
     </div>
 </template>
 
@@ -24,7 +27,8 @@ export default {
   data () {
     return {
       cities: {},
-      hotCities: []
+      hotCities: [],
+      letter: ''
     }
   },
   mounted () {
@@ -40,6 +44,9 @@ export default {
       const data = res.data
       this.cities = data.cities
       this.hotCities = data.hotCities
+    },
+    handleLetterChange (letter) {
+      this.letter = letter
     }
   }
 }
